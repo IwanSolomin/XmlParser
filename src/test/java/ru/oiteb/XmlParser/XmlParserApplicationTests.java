e3cba496f42dcdf4fae07ccacb0af0d8bd7f7831
Badlose <enelz@mail.ru>
2025-12-17 12:32:43 +0300

    Initial commit

---
+++ src/test/java/ru/oiteb/XmlParser/XmlParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oiteb.XmlParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
